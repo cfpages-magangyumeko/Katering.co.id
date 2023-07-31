9bd60630c1694242833d0e0aeece519722125878
Syamsul Alam <109963382+eriko20@users.noreply.github.com>
2023-07-31 16:00:03 +0700

    Update Artikel

---
+++ wp-content/plugins/elementor/assets/js/packages/editor-v1-adapters.asset.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger">
+<strong>QSandbox Fatal error/exception: </strong> <br>File: /wp-content/plugins/elementor/assets/js/packages/editor-v1-adapters.asset.php<br>Line: 10<br>Request Id: 7e2df214caca0f78327ed8d80d8066871d01978f<br>Plugin: elementor<br>
+</div>
+<br>If you think this is an error with the platform <a href="https://qsandbox.com/app/?page=contact&subject=Site+Error%3A+oxygen-q2dy3lb5ldmtl.oxygen-demo.qsandbox.org&message=Hi%2C%0A%0A%0ARequested+URL%3A+%2Fwp-content%2Fplugins%2Felementor%2Fassets%2Fjs%2Fpackages%2Feditor-v1-adapters.asset.php%3Fsimply_static_page%3D213%0AID%3A+7e2df214caca0f78327ed8d80d8066871d01978f" target="_blank">Contact us </a> so we can look into it.
---
+++ wp-content/plugins/elementor/assets/js/packages/env.asset.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger">
+<strong>QSandbox Fatal error/exception: </strong> <br>File: /wp-content/plugins/elementor/assets/js/packages/env.asset.php<br>Line: 10<br>Request Id: ca6ca08165a97c21c18b2b12ffb65e2429b471c5<br>Plugin: elementor<br>
+</div>
+<br>If you think this is an error with the platform <a href="https://qsandbox.com/app/?page=contact&subject=Site+Error%3A+oxygen-q2dy3lb5ldmtl.oxygen-demo.qsandbox.org&message=Hi%2C%0A%0A%0ARequested+URL%3A+%2Fwp-content%2Fplugins%2Felementor%2Fassets%2Fjs%2Fpackages%2Fenv.asset.php%3Fsimply_static_page%3D193%0AID%3A+ca6ca08165a97c21c18b2b12ffb65e2429b471c5" target="_blank">Contact us </a> so we can look into it.
